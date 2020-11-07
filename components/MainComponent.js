c4a13c4d8bcdaa3428846f378409d6814196bc82
Nigel <Ncaba615@gmail.com>
2020-11-07 13:14:35 -0800

    Final

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -31,7 +31,6 @@ import {
 } from "../redux/ActionCreators";
 import NetInfo from "@react-native-community/netinfo";
 
-
 const mapDispatchToProps = {
   fetchCampsites,
   fetchComments,
@@ -335,19 +334,21 @@ class Main extends Component {
     this.props.fetchComments();
     this.props.fetchPromotions();
     this.props.fetchPartners();
-
-    NetInfo.fetch().then((connectionInfo) => {
-      Platform.OS === "ios"
-        ? Alert.alert("Initial Network Connectivity Type:", connectionInfo.type)
-        : ToastAndroid.show(
-            "Initial Network Connectivity Type: " + connectionInfo.type,
-            ToastAndroid.LONG
-          );
-    });
+    this.showNetInfo();
 
     this.unsubscribeNetInfo = NetInfo.addEventListener((connectionInfo) => {
       this.handleConnectivityChange(connectionInfo);
     });
+  }
+
+  async showNetInfo() {
+    const connectionInfo = await NetInfo.fetch();
+    Platform.OS === "ios"
+      ? Alert.alert("Initial Network Connectivity Type:", connectionInfo.type)
+      : ToastAndroid.show(
+          "Initial Network Connectivity Type: " + connectionInfo.type,
+          ToastAndroid.LONG
+        );
   }
 
   componentWillUnmount() {
